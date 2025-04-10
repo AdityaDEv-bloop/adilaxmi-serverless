f1bf3bc18b4d1fcd3c9d5ad9dd8c3aff846b3b90
Meta Code Solution <68818876+AdityaDEv-bloop@users.noreply.github.com>
2025-04-11 00:23:50 +0530

    Auth Update

--- handler/adl_authorizer.py
+++ handler/adl_authorizer.py
@@ -4,7 +4,7 @@ import json
 
 def adlAuthorizer(event, context):
     body = {
-        "message": "Go Serverless v4.0! Your function executed successfully!",
+        "message": "Go Serverless v4.0! Your function executed successfully Auth!",
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
