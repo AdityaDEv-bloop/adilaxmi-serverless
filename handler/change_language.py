3be9eda519080a112a8ac5e6351f9153155d2b2c
Meta Code Solution <68818876+AdityaDEv-bloop@users.noreply.github.com>
2025-04-11 00:27:07 +0530

    Auth Update

--- handler/change_language.py
+++ handler/change_language.py
@@ -2,7 +2,7 @@ import json
 from services.changelanguage import ChangeLanguage
 
 def changeLanguage(event, context):
-
+    
     response = {"statusCode": 200, "body": json.dumps(event)}
 
     return response
