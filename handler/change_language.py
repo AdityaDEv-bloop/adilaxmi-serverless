92681c055e3d4d62058226f34fe14f83fbd526c6
Meta Code Solution <68818876+AdityaDEv-bloop@users.noreply.github.com>
2025-04-11 17:43:08 +0530

    update
    update

--- handler/change_language.py
+++ handler/change_language.py
@@ -2,10 +2,10 @@ import json
 from services.changelanguage_service import ChangeLanguage
 
 def changeLanguage(event, context):
-    content = event.body['content']
-    language = event.body['language']
+    content = event['content']
+    language = event['language']
     changelanguage = ChangeLanguage(language=language,contents=content)
-    tarnslated = changelanguage.get_translate_content()
+    tarnslated = changelanguage.translate_content()
     response = {"statusCode": 200, "body": json.dumps(tarnslated)}
 
     return response
