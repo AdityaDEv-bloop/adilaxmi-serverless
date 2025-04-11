33a1cb1309cf4461ba16caf3d7f039f43875417f
Meta Code Solution <68818876+AdityaDEv-bloop@users.noreply.github.com>
2025-04-11 16:00:22 +0530

    Update

---
+++ README_DEPLOY.md
@@ -0,0 +1,1 @@
+pip install -t python/lib/python3.12/site-packages -r requirement.txt
--- handler/change_language.py
+++ handler/change_language.py
@@ -3,6 +3,6 @@ from services.changelanguage import ChangeLanguage
 
 def changeLanguage(event, context):
     
-    response = {"statusCode": 200, "body": json.dumps(event)}
+    response = {"statusCode": 200, "body": "This Okay"}
 
     return response
